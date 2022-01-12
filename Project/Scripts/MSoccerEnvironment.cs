28133169e41172304ec8482b26bd8eaff13ddd60
李慕天 <57753175+leinace1001@users.noreply.github.com>
2022-01-13 00:35:23 +0800

    Add files via upload

---
+++ Project/Scripts/MSoccerEnvironment.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.MLAgents;
+using UnityEngine;
+namespace Project
+{
+
+    [Serializable]
+    public struct PositionConfig
+    {
+        public string position_name;
+        public float forward_speed_scale;
+        public float lateral_speed_scale;
+        public float kick_power_scale;
+        public float timer_reward;
+    }
+
+    public class MSoccerEnvironment : MonoBehaviour
+    {
+
+    #region Reference
+
+        EnvironmentParameters environmentParameters;
+        public Transform ball_transform;
+        public Transform[] goal_transforms;
+
+    #endregion
+
+    #region Config
+
+        /// <summary>
+        ///     One FixedUpdate -> One step. Reaching max step causes episode reset.
+        /// </summary>
+        public int episode_max_steps = 25000;
+        public float player_base_speed = 2;
+        public float player_base_angular_speed = 100;
+        public float player_kick_power = 200;
+        public float player_touch_ball_reward = 0.1f;
+        public List<PositionConfig> positions;
+
+    #endregion
+
+    #region States
+
+        List<MSoccerPlayerAgent> agents;
+        SimpleMultiAgentGroup[] agent_groups;
+        MSoccerBall soccer_ball;
+        /// <summary>
+        ///     The step timer of current episode.
+        /// </summary>
+        public int cur_step { private set; get; }
+        public float cur_step_ratio => cur_step / (float)episode_max_steps;
+
+    #endregion
+
+    #region UnityEvents
+
+        void Start()
+        {
+            InitEnvironment();
+        }
+
+        void FixedUpdate()
+        {
+            if (cur_step >= episode_max_steps)
+            {
+                EndEpisode( true );
+                InitEpisode();
+            }
+            cur_step++;
+        }
+
+    #endregion
+
+    #region Actions
+
+    #region Initiation
+
+        void InitEnvironment()
+        {
+            environmentParameters = Academy.Instance.EnvironmentParameters;
+            agents = GetComponentsInChildren<MSoccerPlayerAgent>().ToList();
+            agent_groups = new SimpleMultiAgentGroup[2];
+            agent_groups[0] = new SimpleMultiAgentGroup();
+            agent_groups[1] = new SimpleMultiAgentGroup();
+            foreach (var agent in agents)
+            {
+                agent_groups[agent.team_id].RegisterAgent( agent );
+            }
+            soccer_ball = GetComponentInChildren<MSoccerBall>();
+            ball_transform = soccer_ball.transform;
+            goal_transforms = new Transform[2];
+            goal_transforms[0] = transform.Find( "Field" ).Find( "Goal_0" );
+            goal_transforms[1] = transform.Find( "Field" ).Find( "Goal_1" );
+        }
+
+        void InitEpisode()
+        {
+            cur_step = 0;
+            soccer_ball.Init();
+            player_touch_ball_reward = environmentParameters.GetWithDefault( "ball_touch", player_touch_ball_reward );
+        }
+
+    #endregion
+
+        void EndEpisode(bool interrupt)
+        {
+            if (interrupt)
+            {
+                agent_groups[0].GroupEpisodeInterrupted();
+                agent_groups[1].GroupEpisodeInterrupted();
+            }
+            else
+            {
+                agent_groups[0].EndGroupEpisode();
+                agent_groups[1].EndGroupEpisode();
+            }
+
+        }
+
+    #region Interface
+
+        public void Goal(int scored_team_id)
+        {
+            int enemy_team_id = scored_team_id == 0 ? 1 : 0;
+            agent_groups[scored_team_id].AddGroupReward( 1 );
+            agent_groups[enemy_team_id].AddGroupReward( -1 );
+            EndEpisode( false );
+            InitEpisode();
+        }
+
+    #endregion
+
+    #endregion
+
+    }
+}
---
+++ Project/Scripts/MSoccerPlayerAgent.cs
@@ -0,0 +1,185 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+namespace Project
+{
+    public class MSoccerPlayerAgent : Agent
+    {
+    #region Refenrence
+
+        MSoccerEnvironment Environment;
+        BehaviorParameters Parameters;
+        Rigidbody Rigidbody;
+
+    #endregion
+
+    #region Config
+
+        [HideInInspector]
+        public int team_id;
+        int team_id_reverse;
+        public int position_id;
+        PositionConfig m_position_config;
+        Vector3 spawn_position;
+        Quaternion spawn_rotation;
+
+    #endregion
+
+    #region State
+
+        int m_action_kick_power;
+
+    #endregion
+
+    #region AgentEvent
+
+        public override void Initialize()
+        {
+            Parameters = GetComponent<BehaviorParameters>();
+            Environment = GetComponentInParent<MSoccerEnvironment>();
+            team_id = Parameters.TeamId;
+            team_id_reverse = team_id == 0 ? 1 : 0;
+            Rigidbody = GetComponent<Rigidbody>();
+            Rigidbody.maxAngularVelocity = 500;
+            m_position_config = Environment.positions[position_id];
+            var transform1 = transform;
+            spawn_position = transform1.position;
+            spawn_rotation = transform1.rotation;
+
+        }
+
+        public override void CollectObservations(VectorSensor sensor) { }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            var collided = collision.gameObject;
+            if (!collided.CompareTag( "ball" ))
+            {
+                return;
+            }
+            AddReward( Environment.player_touch_ball_reward );
+            var force =
+                m_action_kick_power * Environment.player_kick_power * m_position_config.kick_power_scale *
+                Mathf.Clamp( Vector3.Dot( Rigidbody.velocity, transform.forward ), 0, 10 );
+            var direction = (collision.contacts[0].point - transform.position).normalized;
+            collision.rigidbody.AddForce( direction * force );
+
+        }
+        public override void Heuristic(in ActionBuffers actionsOut)
+        {
+            var acts = actionsOut.DiscreteActions;
+            //forward
+            if (Input.GetKey( KeyCode.W ))
+            {
+                acts[0] = 1;
+            }
+            if (Input.GetKey( KeyCode.S ))
+            {
+                acts[0] = 2;
+            }
+
+            //rotate
+            if (Input.GetKey( KeyCode.E ))
+            {
+                acts[2] = 1;
+            }
+            if (Input.GetKey( KeyCode.Q ))
+            {
+                acts[2] = 2;
+            }
+
+            //right
+            if (Input.GetKey( KeyCode.D ))
+            {
+                acts[1] = 1;
+            }
+            if (Input.GetKey( KeyCode.A ))
+            {
+                acts[1] = 2;
+            }
+
+            if (Input.GetKey( KeyCode.Space ))
+            {
+                acts[3] = 2;
+            }
+            else
+            {
+                acts[3] = 1;
+            }
+        }
+
+        // public override void CollectObservations(VectorSensor sensor) { }
+        public override void OnActionReceived(ActionBuffers actionsBuffers)
+        {
+            AddReward( m_position_config.timer_reward *
+                       Environment.cur_step_ratio );
+
+            var position = Environment.ball_transform.position;
+            /*AddReward( 0.1f / (1 +
+                                 Vector3.Distance(
+                                     position,
+                                     transform.position )) );*/
+            /*AddReward(0.005f*Mathf.Exp(-0.01f*Mathf.Pow(Vector3.Distance(
+                                     position,
+                                     transform.position ),0.5f)));*/
+            /*AddReward(-0.001f*Vector3.Distance(
+                                     position,
+                                     transform.position ));*/
+                                     
+                                     
+
+            /*AddReward( 0.001f / (1 +
+                                 Vector3.Distance(
+                                     Environment.goal_transforms[team_id_reverse].position,
+                                     position )) );*/
+
+            var acts = actionsBuffers.DiscreteActions;
+
+            m_action_kick_power = acts[3];
+
+            Move(
+                b_table[acts[0]],
+                b_table[acts[1]],
+                b_table[acts[2]] );
+        }
+        public override void OnEpisodeBegin()
+        {
+            var pos = spawn_position +
+                      Vector3.right * Random.Range( -5f, 5f );
+            var rot = spawn_rotation *
+                      Quaternion.Euler( 0, Random.Range( -10f, 10f ), 0 );
+            transform.SetPositionAndRotation( pos, rot );
+            Rigidbody.velocity = Vector3.zero;
+            Rigidbody.angularVelocity = Vector3.zero;
+        }
+
+    #endregion
+
+    #region Util
+
+        /// <summary>
+        ///     ori_action_branch_table: old branch number -> new action
+        /// </summary>
+        int[] b_table =
+        {
+            0,
+            1,
+            -1
+        };
+
+        public void Move(int z_axis_move, int x_axis_move, int y_axis_rot)
+        {
+            var transform1 = transform;
+            var z_vector = m_position_config.forward_speed_scale * transform1.forward * z_axis_move;
+            var x_vector = m_position_config.lateral_speed_scale * transform1.right * x_axis_move;
+            var y_vector = transform1.up * -y_axis_rot;
+            transform1.Rotate( y_vector, Time.deltaTime * Environment.player_base_angular_speed );
+            Rigidbody.AddForce( (x_vector + z_vector) * Environment.player_base_speed, ForceMode.VelocityChange );
+        }
+
+    #endregion
+
+    }
+}
